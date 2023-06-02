74360bccddcb17bc9f99530b666cd0f55b38554f
alipeng <alipeng@aliyun.com>
2023-06-02 19:04:50 +0800

    Update completion-page.blade.php

--- resources/views/completion-page.blade.php
+++ resources/views/completion-page.blade.php
@@ -34,7 +34,7 @@
             {{ $title }}
         </div>
         <div class="st-desc">
-            {{ $description }}
+            {!! $description !!}
         </div>
 
         <div class="st-btn">
